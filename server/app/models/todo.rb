211cb285407b5aba4c5fe5652eee2d6713896f14
Bob Oyier <boboyier92@gmail.com>
2023-03-23 11:39:37 +0300

    Todo model complete.

---
+++ server/app/models/todo.rb
@@ -0,0 +1,11 @@
+class Todo < ApplicationRecord
+  belongs_to :user
+
+  enum :status, %i[CREATED STARTED COMPLETED CANCELED]
+  enum :priority, %i[LOW MEDIUM HIGH]
+
+  validates :title, { length: { minimum: 5, maximum: 20 } }, presence: true
+  validates :description, { length: { minimum: 20 } }, presence: true
+  validates :status, numericality: { in: (0..3) }
+  validates :priority, numericality: { in: (0..3) }
+end
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :todos
+
   validates :username,
             { length: { in: 5..8 }, uniqueness: true, presence: true }
   validates :email, { uniqueness: true, presence: true }
